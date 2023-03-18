6ea2067499fb063da81a6ea6158bcdb511c8e153
aryo faizi <aryofaizi@gmail.com>
2023-03-19 00:36:16 +0330

    first four btns created : 7,8,9,plus

--- main.py
+++ main.py
@@ -11,5 +11,27 @@ lbl_result = tkinter.Label(
     height=3,
 )
 
-lbl_result.grid(row=0,column=0)
+btn_7 = tkinter.Button(
+    master=window,
+    text="7",
+)
+btn_8 = tkinter.Button(
+    master=window,
+    text="8",
+)
+btn_9 = tkinter.Button(
+    master=window,
+    text="9",
+)
+btn_plus = tkinter.Button(
+    master=window,
+    text="+",
+)
+lbl_result.grid(row=0,column=0,columnspan=4)
+btn_7.grid(row=1,column=0,sticky=(E,W))
+btn_8.grid(row=1,column=1,sticky=(E,W))
+btn_9.grid(row=1,column=2,sticky=(E,W))
+btn_plus.grid(row=1,column=3,sticky=(E,W))
 window.mainloop()
+
+
